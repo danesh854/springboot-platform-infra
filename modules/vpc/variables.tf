807dce5c21f9194592760ef7f014a23154f1db00
EC2 Default User <ec2-user@ip-172-31-34-71.ap-south-1.compute.internal>
2026-06-02 18:08:05 +0000

    created production vpc terraform module

---
+++ modules/vpc/variables.tf
@@ -0,0 +1,17 @@
+variable "environment" {}
+
+variable "vpc_cidr" {}
+
+variable "public_subnet_cidrs" {
+  type = list(string)
+}
+
+
+variable "private_subnet_cidrs" {
+  type = list(string)
+}
+
+
+variable "azs" {
+  type = list(string)
+}
